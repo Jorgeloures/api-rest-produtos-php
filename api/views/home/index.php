de51a95eb6c983b78db5bdde09f51d8d73359702
Jorgeloures <jelmachado70@gmail.com>
2026-05-20 14:28:54 -0300

    Estilização da tela inicial + criação de botão Listar Categorias + imagem de fundo - inclusão da arquivo png no assets/img

--- api/views/home/index.php
+++ api/views/home/index.php
@@ -1,8 +1,14 @@
-<div class="container">
-  <div class="row">
-    <div class="col-8 offset-2 text-center">
-      <h1>Construindo um simples Framework MVC com PHP</h1>
-      <a href="<?= BASE_URL . '/product' ?>">Listar todos os produtos</a>
+  <div class="container-fluid" 
+      style=" background-image: url('<?= BASE_URL ?>/assets/img/fundo.png'); background-size: cover; background-position: center; min-height: 100vh;" >
+  
+    <div class="row"> 
+      <div class="col-10 offset-1 text-center" style="margin-top:100px">
+        <h1 class="text-uppercase mb-4" style="font-size:60px; color:red;"> Construindo um simples Framework MVC com PHP </h1>
+
+      <div class="d-flex flex-column align-items-center">
+        <a href="<?= BASE_URL . '/product' ?>" class="btn btn-primary" style="width:400px; padding:15px; font-size:18px; margin-bottom:20px;" > LISTAR PRODUTOS </a>
+
+        <a href="<?= BASE_URL . '/categoria' ?>" class="btn btn-primary" style="width:400px; padding:15px; font-size:18px;" > LISTAR CATEGORIAS </a> 
+      </div>
     </div>
   </div>
-</div>
